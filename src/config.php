0463564163730ccc149bf94a34f6af6844818d2f
STougeiro <sidneytougeiro@gmail.com>
2024-10-03 07:19:33 -0300

    Adding mail config file example

---
+++ src/config.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+    return
+    [
+        'host' => '',
+
+        'username' => '',
+
+        'password' => '',
+
+        'port' => '',
+
+        'from' =>
+        [
+            'address' => '',
+
+            'name' => '',
+        ],
+
+        'charset' => 'UTF-8',
+    ];
